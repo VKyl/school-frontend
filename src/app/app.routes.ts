1eba5d2016aab68414cdd34f6f102e5d406341e0
vkyl <viktor.kyliukh@gmail.com>
2025-05-24 11:01:44 +0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: "Manage Participants",
+    path: "manage-participants",
+    loadComponent: () => import('./pages/people-management/people-management.component')
+  }
+];
