dc5a005a03e09db39184fffe8a1177fb4ff388cb
vkyl <viktor.kyliukh@gmail.com>
2025-05-24 10:57:11 +0300

    structure

---
+++ src/app/pages/people-management/people-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './people-management.component.html',
+  styleUrl: './people-management.component.css'
+})
+export default class PeopleManagementComponent {
+
+}
