a532996dfcf1b8eaf340968322b0711a68c1f0df
vkyl <viktor.kyliukh@gmail.com>
2025-05-24 16:47:31 +0300

    prpr pipipim

---
+++ src/app/pages/assigment-helper/assigment-helper.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assigment-helper',
+  imports: [],
+  templateUrl: './assigment-helper.component.html',
+  styleUrl: './assigment-helper.component.css'
+})
+export class AssigmentHelperComponent {
+
+}
